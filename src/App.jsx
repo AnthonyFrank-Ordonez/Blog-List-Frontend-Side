f4de947795fab91c5cd742de4428a2ae2db809ef
Anthony Frank A. Ordoñez <anthonyfrankordonez@yahoo.com>
2025-03-25 21:32:35 +0800

    This is only a test

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import { setUsers } from './reducers/usersReducer'
 const App = () => {
   const { data: blogs, isSuccess: blogSuccess } = useBlogs()
   const { data: users, isSuccess: userSuccess } = useUsers()
+  console.log('🚀 ~ App ~ users:', users)
   const dispatch = useDispatch()
 
   useEffect(() => {
