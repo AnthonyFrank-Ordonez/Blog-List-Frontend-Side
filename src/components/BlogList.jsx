1fc44f16a68ae67322ba6154d1dfe4123d49b40b
Anthony Frank A. Ordoñez <anthonyfrankordonez@yahoo.com>
2025-03-26 14:22:28 +0800

    Add user and blog route

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,59 +1,36 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 // FROM SRC FOLDER
-import { setNotification } from '../reducers/notificationReducer'
-import Blog from './Blog'
 import BlogForm from './BlogForm'
-import { useDeleteBlog, useUpdateBlogVote } from '../hooks'
+import { Link } from 'react-router-dom'
 
 const BlogList = () => {
-  const { mutateAsync: updateBlogLike } = useUpdateBlogVote()
-  const { mutateAsync: deleteBlog } = useDeleteBlog()
-  const dispatch = useDispatch()
-  const { user } = useSelector((state) => state.users)
   const { blogs, isLoading } = useSelector((state) => state.blogs)
 
-  const handleDelete = async (blog) => {
-    const confirm = window.confirm(
-      `Remove blog ${blog.title} by ${blog.author}`
-    )
-
-    if (confirm) {
-      await deleteBlog(blog.id)
-
-      dispatch(
-        setNotification(
-          `Successfully deleted ${blog.title} by ${blog.author}`,
-          5000
-        )
-      )
-    }
-  }
-
-  const handleAddLike = async (blog) => {
-    await updateBlogLike({ ...blog, likes: blog.likes + 1 })
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
   }
 
   if (isLoading) return <div>Loading...</div>
 
   return (
-    <>
-      <h2>Blog App</h2>
-
+    <div>
       <BlogForm />
 
       {[...blogs]
         .sort((blogA, blogB) => blogB.likes - blogA.likes)
         .map((blog) => (
-          <Blog
-            key={blog.id}
-            blog={blog}
-            toggleLike={handleAddLike}
-            toggleDelete={handleDelete}
-            user={user}
-          />
+          <div style={blogStyle} key={blog.id}>
+            <Link to={`/blogs/${blog.id}`}>
+              {blog.title} by {blog.author}
+            </Link>
+          </div>
         ))}
-    </>
+    </div>
   )
 }
 
