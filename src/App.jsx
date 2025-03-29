5f6cb61f49e4daf389aa1b7335792583c38a2687
Anthony Frank A. Ordoñez <anthonyfrankordonez@yahoo.com>
2025-03-29 10:38:27 +0800

    Added error callback function when there is an error fetching blogs or users

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-// Import Services
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-
 // Import Components/From Src folder
 import Notification from './components/Notification'
 import NavigationBar from './components/NavigationBar'
 import AppRoutes from './routes/AppRoutes'
-import { setBlogs } from './reducers/blogReducer'
+
 import { useBlogs, useUsers } from './hooks'
-import { setUsers } from './reducers/usersReducer'
 
 const App = () => {
-  const { data: blogs, isSuccess: blogSuccess } = useBlogs()
-  const { data: users, isSuccess: userSuccess } = useUsers()
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    if (blogs && blogSuccess) dispatch(setBlogs(blogs))
-    if (users && userSuccess) dispatch(setUsers(users))
-  }, [blogs, users, userSuccess, blogSuccess, dispatch])
+  const { isSuccess: blogSuccess, isError: blogError } = useBlogs()
+  const { isSuccess: userSuccess, isError: userError } = useUsers()
 
   return (
-    <div>
-      <Notification />
-      <NavigationBar />
-      <AppRoutes />
-    </div>
+    <>
+      {(blogError || userError) && (
+        <div>Sorry this site is currently unavailable</div>
+      )}
+
+      {blogSuccess && userSuccess && (
+        <div>
+          <Notification />
+          <NavigationBar />
+          <AppRoutes />
+        </div>
+      )}
+    </>
   )
 }
 
